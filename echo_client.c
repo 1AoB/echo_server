33628ab856779bfca3f4bbb3a27561f29d98d2a6
1AoB <78208268+1AoB@users.noreply.github.com>
2023-05-15 00:56:15 +0800

    Update echo_client.c

--- echo_client.c
+++ echo_client.c
@@ -8,6 +8,12 @@
 #define SERVER_PORT 6666
 #define SERVER_IP "127.0.0.1"
 
+/*
+ * 客户端:在客户端中，连接套接字和通信套接字是同一个变量
+ * 连接的套接字:sockfd,使用socket()创建
+ * 通信的套接字:sockfd,connect() 函数将会使用 sockfd 连接到服务器端的套接字
+ */
+
 // argc 是命令行参数的个数，其值包括程序名称和命令行参数。
 
 /*
@@ -20,6 +26,7 @@ argv[3] = "arg3"
 */
 int main(int argc, char *argv[])
 {
+    /********处理main函数的参数*********/
     int sockfd;
     char *message;
     struct sockaddr_in server_addr;
@@ -34,6 +41,8 @@ int main(int argc, char *argv[])
 
     message = argv[1];
     printf("message : %s\n", message);
+    /************************/
+    // 创建连接的套接字
     sockfd = socket(AF_INET, SOCK_STREAM, 0); // SOCK_STREAM:流式传输协议,0:表示使用默认协议。
 
     memset(&server_addr, '\0', sizeof(struct sockaddr_in));
@@ -41,10 +50,13 @@ int main(int argc, char *argv[])
     server_addr.sin_family = AF_INET;
     // inet_ntop这个函数名,n指的是网络直接序的ip,p指的是主机字节序的ip
     // inet_ntop(AF_INET, &client.sin_addr.s_addr, client_ip, sizeof(client_ip))
-    inet_pton(AF_INET, "127.0.0.1", &server_addr.sin_addr);
+    inet_pton(AF_INET,
+              "127.0.0.1",
+              &server_addr.sin_addr);
     // inet_pton(AF_INET, SERVER_IP, &server_addr.sin_addr);
     server_addr.sin_port = htons(SERVER_PORT);
 
+    // 更改为通信的套接字
     if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
     {
         perror("connect failed");
